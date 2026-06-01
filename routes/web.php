8a969c22a652b0e31294cd52352cd09e92ee721b
Fauzi <fauzienhaa@gmail.com>
2026-06-01 23:29:15 +0700

    edit routing dan filesys

--- routes/web.php
+++ routes/web.php
@@ -12,16 +12,19 @@ use App\Http\Controllers\Admin\LipConditionController;
 use App\Http\Controllers\Admin\UndertoneController;
 use App\Http\Controllers\Admin\ProductController;
 
-// User Routes
-Route::get('/', [RecommendationController::class, 'index'])->name('recommendation.form');
-Route::post('/recommendation', [RecommendationController::class, 'process'])->name('recommendation.process');
-
 // Admin Routes
 $adminRouteGroup = env('ADMIN_DOMAIN') 
     ? Route::domain(env('ADMIN_DOMAIN')) 
     : Route::prefix('admin');
 
 $adminRouteGroup->name('admin.')->group(function () {
+    Route::get('/', function () {
+        if (auth()->check()) {
+            return redirect()->route('admin.dashboard');
+        }
+        return redirect()->route('admin.login');
+    });
+
     Route::get('login', [AuthController::class, 'showLoginForm'])->name('login')->middleware('guest');
     Route::post('login', [AuthController::class, 'login'])->name('login.submit')->middleware('guest');
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
@@ -38,3 +41,7 @@ $adminRouteGroup->name('admin.')->group(function () {
         Route::resource('products', ProductController::class);
     });
 });
+
+// User Routes
+Route::get('/', [RecommendationController::class, 'index'])->name('recommendation.form');
+Route::post('/recommendation', [RecommendationController::class, 'process'])->name('recommendation.process');
